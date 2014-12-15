073ce1d54ca8c3ee993ef7d3e598fa88cb22373c
thundervoice <dmbbandola@gmail.com>
2014-12-15 20:26:40 +0800

    modified lines

--- test/bdd/features/scores.feature
+++ test/bdd/features/scores.feature
@@ -3,6 +3,6 @@ Feature: An app that tallies the score of two teams in a game
 		As a scorer, I want to tally the points of the two teams so I can keep track of the winner of the game.
 
 		Scenario: Tally points of the team that scored
-		Given there are two teams
+		Given there are two teams Team A and Team B
 		When a team has scored
-		Then the scoring team's point will be added with the scored point
+		Then the scoring teams point will be added with the scored point
--- test/bdd/steps/scores_steps.py
+++ test/bdd/steps/scores_steps.py
@@ -1,12 +1,16 @@
-import lettuce
-from scores import Scores
-from teams import Team
+from lettuce import step
+from lab.scores import Score
+from lab.teams import Team
+from nose.tools import assert_equal, assert_in
 
-@step(u'Given there are two teams')
-def given_there_are_two_teams(step):
-    team = Team()
-    teamA = Team('Team A', 0)
-    teamB = Team('Team B', 0)
-    team.add_team(teamA)
-    team.add_team(teamB)
-    self.assertEqual(len(lab.teams), 2)
+@step(u'Given there are two teams Team A and Team B')
+def given_there_are_two_teams_team_a_and_team_b(step):
+	assert True
+
+@step(u'When a team has scored')
+def when_a_team_has_scored(step):
+	assert True
+
+@step(u'Then the scoring teams point will be added with the scored point')
+def then_the_scoring_team_s_point_will_be_added_with_the_scored_point(step):
+	assert True
